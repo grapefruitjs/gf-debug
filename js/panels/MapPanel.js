990111b93420b126307a283f3cfc8fdd2bc05960
Chad Engler <cengler@blizzard.com>
2013-08-01 12:34:29 -0700

    no need for that

--- js/panels/MapPanel.js
+++ js/panels/MapPanel.js
@@ -9,13 +9,11 @@ gf.inherits(gf.debug.MapPanel, gf.debug.Panel, {
     createPanelElement: function() {
         var div = gf.debug.Panel.prototype.createPanelElement.call(this);
 
+        this.minimap = new gf.debug.Minimap(div, this.game);
+
         return div;
     },
     tick: function() {
-        if(this.minimap) {
-            this.minimap.render();
-        } else if(this.game.world) {
-            this.minimap = new gf.debug.Minimap(this._panel, this.game);
-        }
+        this.minimap.render();
     }
 });
